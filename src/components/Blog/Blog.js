546202feac079aa1710224bf9adc1c1da1903b2f
Nizam-Shejan11 <www.nizam.shejan11@gmail.com>
2022-10-12 22:02:04 +0600

    qsn

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -10,6 +10,26 @@ const Blog = () => {
         <h1 className="text-center mb-5">
           **THE BLOG LIST** <br />
         </h1>
+        <div className="qsn-ans">
+          <div className="qsn-1">
+            <h2>Question : The Purpose of React Router ??</h2>
+            <h6>
+              Answer : React Router is a standard library for routing in React.
+              It enables the navigation among views of various components in a
+              React Application, allows changing the browser URL, and keeps the
+              UI in sync with the URL.
+            </h6>
+          </div>
+          <br />
+          <br />
+
+          <br />
+          <br />
+
+          <br />
+          <br />
+          <br />
+        </div>
       </div>
       <Footer></Footer>
     </div>
